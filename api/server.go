83122bd730c62ac4c85f42105b2893a1be5ab0f5
Abduhalim <abdulhalim1992@gmail.com>
2023-11-18 12:25:59 +0500

    18.11.2023

--- api/server.go
+++ api/server.go
@@ -23,10 +23,19 @@ func NewServer(store db.Store) *Server {
 		v.RegisterValidation("currency", validatorCurrency)
 	}
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccounts)
-	router.POST("/transfers", server.createTransfer)
+	{
+		router.POST("/users", server.createUser)
+	}
+
+	{
+		router.POST("/accounts", server.createAccount)
+		router.GET("/accounts/:id", server.getAccount)
+		router.GET("/accounts", server.listAccounts)
+	}
+
+	{
+		router.POST("/transfers", server.createTransfer)
+	}
 
 	server.router = router
 
