9e7a266b3aa02d2441d73e51767ba85327b572f8
Abduhalim <abdulhalim1992@gmail.com>
2024-01-02 18:10:52 +0500

    Добавили извлечение информации из gRPC метаданных

---
+++ gapi/metadata.go
@@ -0,0 +1,50 @@
+package gapi
+
+import (
+	"context"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+	"log"
+)
+
+const (
+	grpcGatewayUserAgentHandler = "grpcgateway-user-agent"
+	xForwardedForHeader         = "x-forwarded-for"
+	userAgentHeader             = "user-agent"
+)
+
+type Metadata struct {
+	UserAgent string
+	ClientIP  string
+}
+
+func (s *Server) extractMetadata(ctx context.Context) *Metadata {
+	metaData := &Metadata{}
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		log.Printf("md: %+v\n", md)
+
+		// для получения имя агента HTTP клиента
+		if userAgents := md.Get(grpcGatewayUserAgentHandler); len(userAgents) > 0 {
+			metaData.UserAgent = userAgents[0]
+		}
+
+		// для получения имя агента gRPC клиента
+		if userAgents := md.Get(userAgentHeader); len(userAgents) > 0 {
+			metaData.UserAgent = userAgents[0]
+		}
+
+		// для получения IP - адреса клиента HTTP
+		if clientIPs := md.Get(xForwardedForHeader); len(clientIPs) > 0 {
+			metaData.ClientIP = clientIPs[0]
+		}
+
+	}
+
+	// для получения IP - адреса клиента gRPC
+	if p, ok := peer.FromContext(ctx); ok {
+		metaData.ClientIP = p.Addr.String()
+	}
+
+	return metaData
+}
