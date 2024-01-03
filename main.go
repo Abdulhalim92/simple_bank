2e16148b1388a0d69e5b7f730eaac2e422adeef4
Abduhalim <abdulhalim1992@gmail.com>
2024-01-03 17:55:41 +0500

    Автоматическая генерация OpenAPI документов

--- main.go
+++ main.go
@@ -103,6 +103,10 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	// получение данных с каталога и обработка
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot create listener: ", err)
